c9a3da3d417774be2b819b5edd28760c731febd9
Mateusz Kumkowski <kumkowskimateusz2002@gmail.com>
2023-08-19 00:59:53 -0400

    Simple final product

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+
 const background = document.querySelector(".background")
 const test_text = document.getElementById("text")
 const test_text2 = document.getElementById('size')
@@ -11,6 +12,7 @@ let current_y_axis = airship.style.top
 //!Mouse pos off center
 let mousePos = {x: undefined, y: undefined}
 let windowSize = {height: innerHeight, width: innerWidth}
+test_text2.textContent = `${windowSize.width}, ${windowSize.height}`
 
 
 document.addEventListener('mousemove',(event)=>{
@@ -28,50 +30,6 @@ window.addEventListener('resize',(event)=>{
 })
 
 //Airship Functions
-
-function outOfBoundsCorrector(maxWidth,maxHeight) { 
-     if(current_x_axis >= maxWidth){
-          airship.style.left = 0
-          current_x_axis = 0
-     } 
-     else if(current_x_axis <= 0){
-          airship.style.left = maxWidth - 1
-          current_x_axis = maxWidth - 1
-     }
-     
-     if(current_y_axis >= maxHeight){
-          airship.style.top = 0
-          current_y_axis = 0
-     }
-     else if(current_y_axis <= 0){
-          airship.style.top = maxHeight - 1
-          current_y_axis = maxHeight -1
-     }
- }
-
- function momentumLogic(amount){
-     if(amount >= 250){
-          return 6
-     }
-     else if(amount < 250 && amount >= 200){
-          return 5
-     }
-     else if(amount < 200 && amount >= 150){
-          return 4
-     }
-     else if(amount < 150 && amount >= 100){
-          return 3
-     }
-     else if(amount < 100 && amount >= 40){
-          return 2
-     }
-     else if(amount < 40 && amount >= 20){
-          return 1
-     }
-     else{
-          return 0
-     }
- }
 
  function movementAmountCalculator(){
      movement_x = (mousePos.x - current_x_axis)
@@ -107,7 +65,6 @@ function moveAirship(){
 
      movement = movementAmountCalculator()
 
-     console.log(movement)
      airship.style.top = String(current_y_axis + movement[1]) + "px"
      airship.style.left = String(current_x_axis + movement[0]) + "px"
      current_x_axis += movement[0]
@@ -119,3 +76,7 @@ function moveAirship(){
 }
 
 //Enemy Functions
+
+function moveEnemy(enemy){
+
+}
--- ship.js
+++ ship.js
@@ -1,1 +1,44 @@
-export default first
+function momentumLogic(amount){
+     if(amount >= 250){
+          return 6
+     }
+     else if(amount < 250 && amount >= 200){
+          return 5
+     }
+     else if(amount < 200 && amount >= 150){
+          return 4
+     }
+     else if(amount < 150 && amount >= 100){
+          return 3
+     }
+     else if(amount < 100 && amount >= 40){
+          return 2
+     }
+     else if(amount < 40 && amount >= 0){
+          return 1
+     }
+     else{
+          return 0
+     }
+ }
+
+ function outOfBoundsCorrector(maxWidth,maxHeight) { 
+     if(current_x_axis >= maxWidth){
+          airship.style.left = 0
+          current_x_axis = 0
+     } 
+     else if(current_x_axis <= 0){
+          airship.style.left = maxWidth - 1
+          current_x_axis = maxWidth - 1
+     }
+     
+     if(current_y_axis >= maxHeight){
+          airship.style.top = 0
+          current_y_axis = 0
+     }
+     else if(current_y_axis <= 0){
+          airship.style.top = maxHeight - 1
+          current_y_axis = maxHeight -1
+     }
+ }
+
